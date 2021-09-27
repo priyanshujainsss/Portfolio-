4b1dbc4b6f14f735773e2ec79844a1e24b532ec1
priyanshujainsss <priyanshu.jain@sourcesoftsolutions.com>
2021-09-27 19:37:09 +0530

    almost done

--- script.js
+++ script.js
@@ -2,14 +2,33 @@ $(document).ready(function(){
     $(window).scroll(function(){
        if(this.scrollY>20){
            $("nav").addClass("sticky");
-       
        }
        else{
            $("nav").removeClass("sticky")
-        
-
        }
-    })
+         if(this.scrollY>500){
+             $(".scroll-up-btn").addClass("show")
+         }
+         else{
+             $(".scroll-up-btn").removeClass("show")
+         }
+    });
+//   slide up script
+  $('.scroll-up-btn').click(function(){
+      console.log("called")
+      $('body').animate({scrollTop:0},100);
+  });
+
+  //typed script
+  var typed=new Typed(".typing",{
+      strings:["", 'Designer',' Developer'],
+      typeSpeed:70,
+      backspeed:60,
+      loop:true,
+  })
+
+
+
    //menun button toggling
    $(".menu-btn").click(function(){
        $(".navbar .menu").toggleClass("active")
@@ -38,7 +57,32 @@ $(".carousel").owlCarousel({
         }
     }
 })
-
-
-
 })
+
+$(".send-mail").click(function(){
+    sendEmail()
+})
+
+
+
+
+
+
+
+
+
+function sendEmail() {
+    Email.send({
+      Host: "smtp.gmail.com",
+      Username: "source.priyanshu@gmail.com",
+      Password: "password",
+      To: 'priyanshu.jain@sourcesoftsolutions.com',
+      From: "source.priyanshu@gmail.com",
+      Subject: "Sending Email using javascript",
+      Body: "Well that was easy!!",
+    })
+      .then(function (message) {
+          console.log(message)
+        alert("mail sent successfully")
+      });
+  }
